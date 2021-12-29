63307f1b80b9726748d76da82c2f5a3dad02c12b
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-12-29 14:22:58 +0200

    Major changes

--- deployDB.sql
+++ deployDB.sql
@@ -1,15 +1,14 @@
-CREATE SCHEMA theatre;
-CREATE TABLE theatre.director (
+CREATE TABLE director (
   id BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
   first_name VARCHAR (20) NOT NULL,
   last_name VARCHAR (50) NOT NULL,
   date_of_birth DATE NOT NULL,
   phone VARCHAR (50)
 );
-CREATE TABLE theatre.repertoire (
+CREATE TABLE repertoire (
   month VARCHAR (50) PRIMARY KEY NOT NULL UNIQUE,
   director_id BIGINT,
-  FOREIGN KEY (director_id) REFERENCES theatre.director,
+  FOREIGN KEY (director_id) REFERENCES director,
   CONSTRAINT month_valid CHECK (
     month IN (
       'January',
@@ -27,23 +26,23 @@ CREATE TABLE theatre.repertoire (
     )
   )
 );
-CREATE TABLE theatre.genre (
+CREATE TABLE genre (
   name VARCHAR (50) PRIMARY KEY NOT NULL UNIQUE
 );
-CREATE TABLE theatre.author (
+CREATE TABLE author (
   id BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
   first_name VARCHAR (20) NOT NULL,
   last_name VARCHAR (50) NOT NULL,
   date_of_birth DATE NOT NULL
 );
-CREATE TABLE theatre.producer (
+CREATE TABLE producer (
   id BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
   first_name VARCHAR (20) NOT NULL,
   last_name VARCHAR (50) NOT NULL,
   date_of_birth DATE NOT NULL,
   categorie VARCHAR (50) NOT NULL
 );
-CREATE TABLE theatre.performance (
+CREATE TABLE performance (
   id BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
   timestamp TIMESTAMP WITH TIME ZONE,
   name VARCHAR (50) NOT NULL,
@@ -55,53 +54,53 @@ CREATE TABLE theatre.performance (
   conductor_id BIGINT,
   artist_id BIGINT,
   author_id BIGINT,
-  FOREIGN KEY (repertoire_month) REFERENCES theatre.repertoire,
-  FOREIGN KEY (genre) REFERENCES theatre.genre,
-  FOREIGN KEY (director_id) REFERENCES theatre.producer,
-  FOREIGN KEY (conductor_id) REFERENCES theatre.producer,
-  FOREIGN KEY (artist_id) REFERENCES theatre.producer,
-  FOREIGN KEY (author_id) REFERENCES theatre.author,
+  FOREIGN KEY (repertoire_month) REFERENCES repertoire,
+  FOREIGN KEY (genre) REFERENCES genre,
+  FOREIGN KEY (director_id) REFERENCES producer,
+  FOREIGN KEY (conductor_id) REFERENCES producer,
+  FOREIGN KEY (artist_id) REFERENCES producer,
+  FOREIGN KEY (author_id) REFERENCES author,
   CONSTRAINT age_limit_valid CHECK (
     age_limit > 0
     AND age_limit <= 18
   )
 );
-CREATE TABLE theatre.actor (
+CREATE TABLE actor (
   id BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
   first_name VARCHAR (20) NOT NULL,
   last_name VARCHAR (50) NOT NULL,
   date_of_birth DATE NOT NULL,
   is_a_student BOOLEAN DEFAULT false
 );
-CREATE TABLE theatre.student (
+CREATE TABLE student (
   grade_book_number BIGINT PRIMARY KEY NOT NULL UNIQUE,
   actor_id BIGINT NOT NULL UNIQUE,
-  FOREIGN KEY (actor_id) REFERENCES theatre.actor
+  FOREIGN KEY (actor_id) REFERENCES actor
 );
-CREATE TABLE theatre.rank (
+CREATE TABLE rank (
   name VARCHAR (50) PRIMARY KEY NOT NULL UNIQUE,
   date DATE NOT NULL,
   actor_id BIGINT NOT NULL,
-  FOREIGN KEY (actor_id) REFERENCES theatre.actor
+  FOREIGN KEY (actor_id) REFERENCES actor
 );
-CREATE TABLE theatre.contest (
+CREATE TABLE contest (
   name VARCHAR (50) PRIMARY KEY NOT NULL UNIQUE,
   place VARCHAR (50),
   actor_id BIGINT NOT NULL,
-  FOREIGN KEY (actor_id) REFERENCES theatre.actor
+  FOREIGN KEY (actor_id) REFERENCES actor
 );
-CREATE TABLE theatre.understudy (
+CREATE TABLE understudy (
   id BIGSERIAL PRIMARY KEY NOT NULL UNIQUE,
   first_name VARCHAR (20) NOT NULL,
   last_name VARCHAR (50) NOT NULL,
   date_of_birth DATE NOT NULL,
   actor_id BIGINT NOT NULL,
-  FOREIGN KEY (actor_id) REFERENCES theatre.actor
+  FOREIGN KEY (actor_id) REFERENCES actor
 );
-CREATE TABLE theatre.role (
+CREATE TABLE role (
   role VARCHAR (50) PRIMARY KEY NOT NULL UNIQUE,
   actor_id BIGINT NOT NULL,
   performance_id BIGINT NOT NULL,
-  FOREIGN KEY (actor_id) REFERENCES theatre.actor,
-  FOREIGN KEY (performance_id) REFERENCES theatre.performance
+  FOREIGN KEY (actor_id) REFERENCES actor,
+  FOREIGN KEY (performance_id) REFERENCES performance
 );
--- selectExamples.sql
+++ selectExamples.sql
@@ -1,28 +1,81 @@
-SELECT * FROM performance;
+---1
+SELECT * FROM performance WHERE id = 4;
+---2
 SELECT * FROM performance WHERE age_limit < 18;
+---3
 SELECT * FROM performance WHERE age_limit < 18 AND genre = 'Comedy' ORDER BY age_limit;
-SELECT * FROM performance WHERE age_limit > 18 OR genre = 'Trategy';
+---4
+SELECT * FROM performance WHERE age_limit > 18 OR genre = 'Tradegy';
+---5
 SELECT * FROM actor WHERE NOT is_a_student;
-SELECT name, age_limit, timestamp FROM performance WHERE repertoire_month = 'December' AND NOT age_limit < 3 ORDER BY timestamp;
+---6
+SELECT name, age_limit, timestamp 
+FROM performance WHERE repertoire_month = 'December' AND NOT age_limit < 3 ORDER BY timestamp;
+---7
 SELECT COUNT(*) AS actors_count FROM actor;
+---8
 SELECT AVG(age_limit) AS average_age_limit FROM performance;
+---9
 SELECT * FROM performance WHERE repertoire_month IN ('December', 'January');
+---10
 SELECT * FROM performance WHERE age_limit BETWEEN 6 AND 12;
+---11
 SELECT * FROM performance WHERE name LIKE 'Alice%';
+---12
 SELECT * FROM performance WHERE name SIMILAR TO '%(in|on)%';
+---13
 SELECT * FROM performance WHERE artist_id IS NOT NULL;
-
-
----
+---14
+SELECT * FROM performance WHERE age_limit IS NULL;
+---15
+SELECT * FROM role WHERE actor_id IS NULL;
+---16-17
 SELECT * FROM (SELECT month FROM repertoire WHERE NOT month = 'January') AS new_table;
+---18-19
 SELECT * FROM actor WHERE EXISTS (SELECT * FROM performance WHERE age_limit < 3);
+---20-21
 SELECT * FROM actor WHERE id IN (SELECT actor_id FROM role WHERE actor_id IS NOT NULL);
+---22-23
 SELECT * FROM (SELECT name AS performance_name FROM performance) AS new_table CROSS JOIN genre;
-SELECT name, role, first_name, last_name FROM role JOIN actor ON role.actor_id = actor.id JOIN performance ON performance.id = role.performance_id;
-SELECT name, role, first_name, last_name FROM role JOIN actor ON role.actor_id = actor.id JOIN performance ON performance.id = role.performance_id AND performance.age_limit > 3;
+---24
+SELECT name, role, first_name, last_name 
+FROM role JOIN actor ON role.actor_id = actor.id JOIN performance ON performance.id = role.performance_id;
+---25
+SELECT name, role, first_name, last_name 
+FROM role JOIN actor ON role.actor_id = actor.id 
+JOIN performance ON performance.id = role.performance_id AND performance.age_limit > 3;
+---26
 SELECT name AS contest_name, first_name, last_name FROM contest JOIN actor ON contest.actor_id = actor.id;
-SELECT * FROM role LEFT JOIN actor ON role.actor_id = actor.id;
-SELECT * FROM role RIGHT JOIN actor ON role.actor_id = actor.id;
+---27-28
+SELECT first_name, last_name, understudy_first_name, understudy_last_name FROM (
+  SELECT actor_id, first_name AS understudy_first_name, last_name AS understudy_last_name FROM understudy
+) AS new_table JOIN actor ON new_table.actor_id = actor.id;
+---29
+SELECT role, name AS performance_name, first_name, last_name
+FROM role LEFT JOIN actor ON role.actor_id = actor.id LEFT JOIN performance ON role.performance_id = performance.id;
+---30
+SELECT role, name AS performance_name, first_name, last_name 
+FROM role RIGHT JOIN actor ON role.actor_id = actor.id RIGHT JOIN performance ON role.performance_id = performance.id;
+---31-33
 SELECT first_name, last_name, date_of_birth FROM actor 
 UNION 
-SELECT first_name, last_name, date_of_birth FROM understudy;
+SELECT first_name, last_name, date_of_birth FROM understudy
+UNION
+SELECT first_name, last_name, date_of_birth FROM author;
+--- Виправлення:
+--- підзапит
+SELECT concat(first_name, ' ', last_name) AS actor, (SELECT AVG(age_limit) AS avg_age_limit FROM
+    (performance JOIN role ON role.performance_id = performance.id) WHERE role.actor_id = actor.id)
+FROM actor;
+SELECT (SELECT concat(first_name, ' ', last_name) AS actor
+        FROM actor WHERE role.actor_id = actor.id) AS actor,
+       (SELECT name FROM performance WHERE role.performance_id = performance.id) AS performance,
+       role
+FROM role;
+--- EXISTS
+SELECT * FROM actor WHERE NOT EXISTS (SELECT * FROM role WHERE role.actor_id = actor.id);
+--- поєднання за рівністю
+SELECT * FROM role, actor, performance
+WHERE role.actor_id = actor.id AND role.performance_id = performance.id;
+SELECT * FROM role, actor, performance
+WHERE role.actor_id = actor.id AND role.performance_id = performance.id AND age_limit < 18;
